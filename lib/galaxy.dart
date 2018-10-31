1b86c319d7f0635a4ec0cfcadd5aa395d4ee0605
AndrewL188 <ajluo2@illinois.edu>
2018-10-31 16:02:18 -0500

    Set background of galaxy to empty galaxy image

--- lib/galaxy.dart
+++ lib/galaxy.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class Galaxy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return new Scaffold(
+      body: new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("assets/galaxyscreen/emptyGalaxy.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
   }
 }
