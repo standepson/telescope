c0070ad752c68b506bcf1a4037bd706d526b4515
AndrewL188 <ajluo2@illinois.edu>
2018-11-06 16:41:11 -0600

    Wrote task input screen, and editted yaml file to include background image

---
+++ lib/task_input_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class TaskEntering extends StatefulWidget {
+  @override
+  TaskEnteringInput createState() => new TaskEnteringInput();
+}
+
+class TaskEnteringInput extends State<TaskEntering> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Enter new task"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("assets/daysOfTheWeekScreens/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Task name:',
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Number of hours:',
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Deadline date:',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),);
+  }
+}
+
+
