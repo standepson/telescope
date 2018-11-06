91220d35a9c5e98b4f948d24c1c69252e2425ba2
AndrewL188 <ajluo2@illinois.edu>
2018-11-05 23:11:48 -0600

    Added path to galaxy images to yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import './galaxy.dart';
+void main() => runApp(new MyApp());
 
-void main() => runApp(new TelescopeApp());
-
-class TelescopeApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: Scaffold(
