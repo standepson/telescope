635b3ed0ae2084ca7a6d1e07fcf424e65c2d77a5
AndrewL188 <ajluo2@illinois.edu>
2018-11-06 00:31:37 -0600

    Added overlayed star images onto background galaxy image

--- lib/galaxy.dart
+++ lib/galaxy.dart
@@ -1,15 +1,100 @@
 import 'package:flutter/material.dart';
 
 class Galaxy extends StatelessWidget {
+  var star_size = 20.0;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Container(
         decoration: new BoxDecoration(
           image: new DecorationImage(
-            image: new AssetImage("assets/galaxyscreen/emptyGalaxy.png"),
+            image: new AssetImage("assets/galaxyScreen/emptyGalaxy.png"),
             fit: BoxFit.cover,
           ),
+        ),
+        child: new Stack(
+          children: <Widget>[
+            new Positioned(
+              left: 200.0,
+              bottom: 300.0,
+              child: new Image.asset(
+                'assets/galaxyScreen/star.png',
+                width: star_size,
+                height: star_size,
+                fit: BoxFit.cover,
+              ),
+            ),
+            new Positioned(
+              left: 180.0,
+              bottom: 200.0,
+              child: new Image.asset(
+                'assets/galaxyScreen/star.png',
+                width: star_size,
+                height: star_size,
+                fit: BoxFit.cover,
+              ),
+            ),
+            new Positioned(
+              left: 290.0,
+              bottom: 270.0,
+              child: new Image.asset(
+                'assets/galaxyScreen/star.png',
+                width: star_size,
+                height: star_size,
+                fit: BoxFit.cover,
+              ),
+            ),
+            new Positioned(
+              left: 100.0,
+              bottom: 400.0,
+              child: new Image.asset(
+                'assets/galaxyScreen/star.png',
+                width: star_size,
+                height: star_size,
+                fit: BoxFit.cover,
+              ),
+            ),
+            new Positioned(
+              left: 60.0,
+              bottom: 230.0,
+              child: new Image.asset(
+                'assets/galaxyScreen/star.png',
+                width: star_size,
+                height: star_size,
+                fit: BoxFit.cover,
+              ),
+            ),
+            new Positioned(
+              left: 255.0,
+              bottom: 155.0,
+              child: new Image.asset(
+                'assets/galaxyScreen/star.png',
+                width: star_size,
+                height: star_size,
+                fit: BoxFit.cover,
+              ),
+            ),
+            new Positioned(
+              left: 260.0,
+              bottom: 330.0,
+              child: new Image.asset(
+                'assets/galaxyScreen/star.png',
+                width: star_size,
+                height: star_size,
+                fit: BoxFit.cover,
+              ),
+            ),
+            new Positioned(
+              left: 290.0,
+              bottom: 410.0,
+              child: new Image.asset(
+                'assets/galaxyScreen/star.png',
+                width: star_size,
+                height: star_size,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
         ),
       ),
     );
