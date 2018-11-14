da599c1f0fca81cc3a917da3a7ec976d688a5ae5
smzeng <stacymz2@illinois.edu>
2018-11-14 15:00:11 -0600

    added gradient background to calendar file

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -2,16 +2,40 @@ import 'package:flutter/material.dart';
 
 class Calendar extends StatefulWidget {
   @override
-  createState() => CalendarWidget;
+  createState() => new CalendarWidget();
 }
 class CalendarWidget extends State<Calendar> {
+  final darkBlue = const Color(0xFF151d4b);
+  final mediumBlue = const Color(0xFF677191);
+  final blue = const Color(0xFFB0BBCF);
+  final lightBlue = const Color(0xFFDEEAF6);
+
   @override
   Widget build(BuildContext context) {
-    Center(
-      child:Container(
-        child: Image.asset('assets/daysOfTheWeekScreens/background.png');
-        ,
-      ),
+    return Scaffold(
+        body: Container(
+          // Add box decoration
+          decoration: BoxDecoration(
+            // Box decoration takes a gradient
+            gradient: LinearGradient(
+              // Where the linear gradient begins and ends
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              // Add one stop for each color. Stops should increase from 0 to 1
+              stops: [0.1, 0.5, 0.7, 0.9],
+              colors: [
+                // Colors are easy thanks to Flutter's Colors class.
+                darkBlue,
+                mediumBlue,
+                blue,
+                lightBlue
+              ],
+            ),
+          ),
+          child: Column(
+
+          ),
+        )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import './galaxy.dart';
+import './calendar.dart';
 import './home_widget.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  //colors for background
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
