493038ed5ee9f47d4603f074548524f3ce4eccc4
AndrewL188 <ajluo2@illinois.edu>
2018-11-14 16:59:53 -0600

    Added splash page

---
+++ lib/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/Home');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage("assets/splashPage.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
