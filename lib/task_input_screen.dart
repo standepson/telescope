6d70e0d3be93bd367a3f4ccb8177bb6fa55c4bcc
smzeng <stacymz2@illinois.edu>
2018-12-01 20:42:52 -0600

    updated add task page UI

--- lib/task_input_screen.dart
+++ lib/task_input_screen.dart
@@ -4,45 +4,89 @@ class TaskEntering extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Enter new task", ),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: new Container(
-        decoration: new BoxDecoration(
-          image: new DecorationImage(
-            image: new AssetImage("assets/daysOfTheWeekScreens/background.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Task name:',
-                ),
+    return Scaffold(
+        body: Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage(
+                    "assets/daysOfTheWeekScreens/background.png"),
+                fit: BoxFit.cover,
               ),
-              TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Number of hours:',
-                ),
+            ),
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 25.0),
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/inputTaskScreenTitle.png'),
+                  Container(height: (MediaQuery.of(context).size.height) * .05),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child:
+                    Text('Task Name:',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            fontFamily: 'PrintClearly',
+                            color: Colors.white)),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                          fontSize: 18.0,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'PrintClearly',
+                          color: Colors.white),
+                      border: InputBorder.none,
+                      hintText: 'enter name',
+                    ),
+                  ),
+                  Container(height: (MediaQuery.of(context).size.height) * .01),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child:
+                    Text('Number of Hours:',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            fontFamily: 'PrintClearly',
+                            color: Colors.white)),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                          fontSize: 18.0,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'PrintClearly',
+                          color: Colors.white),
+                      border: InputBorder.none,
+                      hintText: 'enter number',
+                    ),
+                  ),
+                  Container(height: (MediaQuery.of(context).size.height) * .01),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child:
+                    Text('Deadline Date:',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            fontFamily: 'PrintClearly',
+                            color: Colors.white)),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                          fontSize: 18.0,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'PrintClearly',
+                          color: Colors.white),
+                      border: InputBorder.none,
+                      hintText: ' select date',
+                    ),
+                  ),
+                  RaisedButton(color: Colors.white, onPressed: null, child: Text('Add Task'))
+                ],
               ),
-              TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Deadline date:',
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),);
+            )));
   }
 }
-
-
