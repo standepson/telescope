dfcaa4f90c24b07e9d301d74a12c5c4bcee0e596
AndrewL188 <ajluo2@illinois.edu>
2018-12-01 23:30:09 -0600

    Added a task widget, days of the week screen will now display a list of tasks

---
+++ lib/task_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Task extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new FlatButton(
+      onPressed: null,
+      child: Text(
+        "Test Button",
+        style: TextStyle(fontSize: 30.0),
+      ),
+    );
+  }
+}
