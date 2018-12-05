f678071b799c708c5126fa7c9747882910c63643
smzeng <stacymz2@illinois.edu>
2018-12-05 15:42:04 -0600

    edited task input screen

--- lib/task_input_screen.dart
+++ lib/task_input_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TaskEntering extends StatelessWidget {
+  List<DropdownMenuItem<int>> listDrop = [];
+  void loadData() {
+    listDrop.add(new DropdownMenuItem(child: new Text('1 day'), value: 1,));
+    listDrop.add(new DropdownMenuItem(child: new Text('2 days'), value: 2,));
+    listDrop.add(new DropdownMenuItem(child: new Text('3 days'), value: 3,));
+    listDrop.add(new DropdownMenuItem(child: new Text('4 days'), value: 4,));
+    listDrop.add(new DropdownMenuItem(child: new Text('5 days'), value: 5,));
+    listDrop.add(new DropdownMenuItem(child: new Text('6 days'), value: 6,));
+    listDrop.add(new DropdownMenuItem(child: new Text('7 days'), value: 7,));
+  }
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    loadData();
     return Scaffold(
         body: Container(
             decoration: new BoxDecoration(
@@ -14,9 +24,14 @@ class TaskEntering extends StatelessWidget {
               ),
             ),
             child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
               padding: const EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 25.0),
-              child: Column(
-                children: <Widget>[
+              child:
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget> [
                   Image.asset('assets/inputTaskScreenTitle.png'),
                   Container(height: (MediaQuery.of(context).size.height) * .05),
                   Container(
@@ -52,6 +67,7 @@ class TaskEntering extends StatelessWidget {
                             color: Colors.white)),
                   ),
                   TextField(
+                    keyboardType: TextInputType.number,
                     decoration: InputDecoration(
                       hintStyle: TextStyle(
                           fontSize: 18.0,
@@ -66,25 +82,13 @@ class TaskEntering extends StatelessWidget {
                   Container(
                     width: MediaQuery.of(context).size.width,
                     child:
-                    Text('Deadline:',
+                    Text('Deadline in :',
                         textAlign: TextAlign.start,
                         style: TextStyle(
                             fontSize: 24.0,
                             fontFamily: 'PrintClearly',
                             color: Colors.white)),
                   ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintStyle: TextStyle(
-                          fontSize: 18.0,
-                          fontStyle: FontStyle.italic,
-                          fontFamily: 'PrintClearly',
-                          color: Colors.white),
-                      border: InputBorder.none,
-                      hintText: ' select date',
-                    ),
-                  ),
-                  RaisedButton(color: Colors.white, onPressed: null, child: Text('Add Task'))
                 ],
               ),
             )));
