2a2dd4da1f9d56724988315c4b6e3f6ac6266d5b
wzhu8 <wzhu00@gmail.com>
2018-12-05 18:10:31 -0600

    made calendar screen neater????

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -14,143 +14,158 @@ class CalendarWidget extends State<Calendar> {
 
   @override
   Widget build(BuildContext context) {
+    double radius= (MediaQuery.of(context).size.width) * .1666;
+    double scrwidth= (MediaQuery.of(context).size.width);
+    double center= scrwidth * .5;
     return Scaffold(
         body: Container(
           padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 25.0),
-      // Add box decoration
-      decoration: BoxDecoration(
-        // Box decoration takes a gradient
-        gradient: LinearGradient(
-          // Where the linear gradient begins and ends
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          // Add one stop for each color. Stops should increase from 0 to 1
-          stops: [0.1, 0.5, 0.7, 0.9],
-          colors: [
-            darkBlue,
-            mediumBlue,
-            blue,
-            lightBlue
-          ],
-        ),
-      ),
-      child: Column(
-        children: <Widget>[
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                FlatButton(
+          // Add box decoration
+          decoration: BoxDecoration(
+            // Box decoration takes a gradient
+            gradient: LinearGradient(
+              // Where the linear gradient begins and ends
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              // Add one stop for each color. Stops should increase from 0 to 1
+              stops: [0.1, 0.5, 0.7, 0.9],
+              colors: [
+                darkBlue,
+                mediumBlue,
+                blue,
+                lightBlue
+              ],
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              new Positioned(
+                left: center - .4339* radius,
+                top: center - .90097 * radius,
+                child: FlatButton(
                     onPressed: () {
                       Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => DayOfTheWeek("Sunday"))
+                          context,
+                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Sunday"))
                       );
                     },
                     child: Image.asset(
                       'assets/weekCalendarScreen/sundayMoon.png',
-                      width: (MediaQuery.of(context).size.width) * .20,
-                      height: (MediaQuery.of(context).size.width) * .20,
+                      width: scrwidth * .20,
+                      height: scrwidth * .20,
                     )),
-                FlatButton(
+              ),
+              new Positioned(
+                left: center + .4339* radius,
+                top: center - .90097 * radius,
+                child: FlatButton(
                     onPressed: () {
                       Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Monday"))
+                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Sunday"))
                       );
                     },
                     child: Image.asset(
-                        'assets/weekCalendarScreen/mondayMoon.png',
-                        width: (MediaQuery.of(context).size.width) * .20,
-                        height: (MediaQuery.of(context).size.width) * .20,))
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              children: <Widget>[
-                FlatButton(
+                      'assets/weekCalendarScreen/mondayMoon.png',
+                      width: scrwidth * .20,
+                      height: scrwidth * .20,
+                    )),
+              ),
+              new Positioned(
+                left: center - .9749* radius,
+                top: center - .2225 * radius,
+                child: FlatButton(
                     onPressed: () {
                       Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Saturday"))
+                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Sunday"))
                       );
                     },
                     child: Image.asset(
                       'assets/weekCalendarScreen/saturdayMoon.png',
-                      width: (MediaQuery.of(context).size.width) * .20,
-                      height: (MediaQuery.of(context).size.width) * .20,
+                      width: scrwidth * .20,
+                      height: scrwidth * .20,
                     )),
-                FlatButton(
+              ),
+              new Positioned(
+                left: center,
+                top: center,
+                child: FlatButton(
                     onPressed: null,
                     child: Image.asset(
                       'assets/weekCalendarScreen/yourWeek.png',
                       width: (MediaQuery.of(context).size.width) * .30,
                       height: (MediaQuery.of(context).size.width) * .30,
                     )),
-                FlatButton(
+              ),
+              new Positioned(
+                left: center + .9749* radius,
+                top: center - .2225 * radius,
+                child: FlatButton(
                     onPressed: () {
                       Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Tuesday"))
+                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Sunday"))
                       );
                     },
                     child: Image.asset(
                       'assets/weekCalendarScreen/tuesdayMoon-07.png',
-                      width: (MediaQuery.of(context).size.width) * .20,
-                      height: (MediaQuery.of(context).size.width) * .20,
-                    ))
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB((MediaQuery.of(context).size.width) * .03, 0.0, (MediaQuery.of(context).size.width) * .03, 0.0),
-            child: Row(
-              children: <Widget>[
-                FlatButton(
+                      width: scrwidth * .20,
+                      height: scrwidth * .20,
+                    )),
+              ),
+              new Positioned(
+                left: center - .7818* radius,
+                top: center + .6235 * radius,
+                child: FlatButton(
                     onPressed: () {
                       Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Friday"))
+                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Sunday"))
                       );
                     },
                     child: Image.asset(
-                        'assets/weekCalendarScreen/fridayMoon.png',
-                    width: (MediaQuery.of(context).size.width) * .20,
-                    height: (MediaQuery.of(context).size.width) * .20,)
-                ),
-                FlatButton(
+                      'assets/weekCalendarScreen/fridayMoon.png',
+                      width: scrwidth * .20,
+                      height: scrwidth * .20,
+                    )),
+              ),
+              new Positioned(
+                left: center + .7818* radius,
+                top: center + .6235 * radius,
+                child: FlatButton(
                     onPressed: () {
                       Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Thursday"))
+                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Sunday"))
                       );
                     },
                     child: Image.asset(
-                        'assets/weekCalendarScreen/thursdayMoon.png',
-                    width: (MediaQuery.of(context).size.width) * .20,
-                    height: (MediaQuery.of(context).size.width) * .20,
-                    )
-                ),
-               FlatButton(
-                   onPressed: () {
-                     Navigator.push(
-                         context,
-                         MaterialPageRoute(builder: (context) => DayOfTheWeek("Wednesday"))
-                     );
-                   },
-                   child: Image.asset(
-                       'assets/weekCalendarScreen/wednesdayMoon-07.png',
-                   width: (MediaQuery.of(context).size.width) * .20,
-                   height: (MediaQuery.of(context).size.width) * .20,)
-               )
-              ],
-            ),
+                      'assets/weekCalendarScreen/wednesdayMoon-07.png',
+                      width: scrwidth * .20,
+                      height: scrwidth * .20,
+                    )),
+              ),
+              new Positioned(
+                left: center,
+                top: center + radius,
+                child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => DayOfTheWeek("Sunday"))
+                      );
+                    },
+                    child: Image.asset(
+                      'assets/weekCalendarScreen/thursdayMoon.png',
+                      width: scrwidth * .20,
+                      height: scrwidth * .20,
+                    )),
+              ),
+              Image.asset('assets/weekCalendarScreen/key.png', height: (MediaQuery.of(context).size.width) * .40, width: (MediaQuery.of(context).size.width) * .50),
+            ],
           ),
-          Image.asset('assets/weekCalendarScreen/key.png', height: (MediaQuery.of(context).size.width) * .40, width: (MediaQuery.of(context).size.width) * .50),
-        ],
-      ),
-    )
+        )
     );
   }
 }
