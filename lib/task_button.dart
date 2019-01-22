b6c35f261a37c59931afb09b99e33cc9d8193d52
AndrewL188 <ajluo2@illinois.edu>
2019-01-22 00:12:22 -0600

    Tasks for the week are now stored in a global file

--- lib/task_button.dart
+++ lib/task_button.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import './timer.dart';
 
 class Task extends StatelessWidget {
-  String _TaskName;
-  int _TaskDuration;
-  Task(this._TaskName, this._TaskDuration);
+  String _taskName;
+  String get taskName => _taskName;
+  int _taskDuration;
+  int get taskDuration => _taskDuration;
+  Task(this._taskName, this._taskDuration);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -12,11 +14,11 @@ class Task extends StatelessWidget {
       onPressed: () {
         Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => TimerScreen(_TaskDuration))
+            MaterialPageRoute(builder: (context) => TimerScreen(_taskDuration))
         );
       },
       child: Text(
-        _TaskName + ": " + _TaskDuration.toString() + " minutes",
+        _taskName + ": " + _taskDuration.toString() + " minutes",
         style: TextStyle(fontSize: 30.0),
       ),
     );
