c3c71cb8b3b1812a4c86d898f066aa6dc97b3a81
smzeng <stacymz2@illinois.edu>
2019-02-15 22:37:33 -0600

    edited task entering screen

--- lib/task_input_screen.dart
+++ lib/task_input_screen.dart
@@ -54,7 +54,7 @@ class TaskEntering extends State<TaskInput> {
             decoration: new BoxDecoration(
               image: new DecorationImage(
                 image: new AssetImage(
-                    "assets/daysOfTheWeekScreens/background.png"),
+                  'assets/taskInputBackground.png', ),
                 fit: BoxFit.cover,
               ),
             ),
@@ -63,6 +63,7 @@ class TaskEntering extends State<TaskInput> {
               height: MediaQuery.of(context).size.height,
               padding: const EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 25.0),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
@@ -70,14 +71,15 @@ class TaskEntering extends State<TaskInput> {
                   Container(height: (MediaQuery.of(context).size.height) * .05),
                   Container(
                     width: MediaQuery.of(context).size.width,
-                    child: Text('Task Name:',
-                        textAlign: TextAlign.start,
+                    child: Text('TASK NAME:',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                            fontSize: 20.0,
+                            fontSize: 25.0,
                             fontFamily: 'PrintClearly',
-                            color: Colors.white)),
+                            color: const Color(0xFFDEEAF6))),
                   ),
                   TextField(
+                    style: new TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'PrintClearly'),
                     controller: nameController,
                     decoration: InputDecoration(
                       hintStyle: TextStyle(
@@ -85,21 +87,24 @@ class TaskEntering extends State<TaskInput> {
                           fontStyle: FontStyle.italic,
                           fontFamily: 'PrintClearly',
                           color: Colors.white),
-                      border: InputBorder.none,
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.white)
+                      ),
                       hintText: 'enter name',
                     ),
                   ),
                   Container(height: (MediaQuery.of(context).size.height) * .01),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Text('Total Number of Hours Needed:',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'PrintClearly',
-                            color: Colors.white)),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Text('TIME NEEDED TO COMPLETE TASK:',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 25.0,
+                                fontFamily: 'PrintClearly',
+                                color: Colors.white)),
                   ),
                   TextField(
+                    style: new TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'PrintClearly'),
                     controller: hoursController,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
@@ -108,36 +113,43 @@ class TaskEntering extends State<TaskInput> {
                           fontStyle: FontStyle.italic,
                           fontFamily: 'PrintClearly',
                           color: Colors.white),
-                      border: InputBorder.none,
+                      enabledBorder: const UnderlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.white)
+                      ),
                       hintText: 'enter number',
                     ),
                   ),
                   Container(height: (MediaQuery.of(context).size.height) * .01),
                   Container(
                     width: MediaQuery.of(context).size.width,
-                    child: Text('Number of Days till Deadline:',
-                        textAlign: TextAlign.start,
+                    child: Text('DAYS UNTILL DEADLINE:',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                            fontSize: 20.0,
+                            fontSize: 25.0,
                             fontFamily: 'PrintClearly',
                             color: Colors.white)),
                   ),
-                  Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: DropdownButton(
+                  new Theme(
+                    data: Theme.of(context).copyWith(
+                        canvasColor: Theme.of(context).primaryColor
+                    ),
+                      child:DropdownButton(
+                          style: new TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'PrintClearly'),
                           items: listDrop,
                           value: globals.numDays,
                           hint: Text("select num of days",
                               style: TextStyle(
-                                  fontSize: 18.0,
+                                  fontSize: 20.0,
                                   fontStyle: FontStyle.italic,
                                   fontFamily: 'PrintClearly',
                                   color: Colors.white)),
                           onChanged: (value) {
                             globals.numDays = value;
                             setState(() {});
-                          })),
+                          })
+                  ),
                   Container(
+                    alignment: Alignment.center,
                     child: RaisedButton(
                       color: Colors.white,
                       onPressed: () =>  setState(() {_saveTask(context);}),
@@ -146,7 +158,7 @@ class TaskEntering extends State<TaskInput> {
                         style: TextStyle(
                             fontFamily: 'PrintClearly',
                             fontSize: 24.0,
-                            color: Colors.deepPurpleAccent),
+                            color: Colors.deepPurple),
                       ),
                     ),
                   )
@@ -157,6 +169,18 @@ class TaskEntering extends State<TaskInput> {
 }
 
 void _saveTask (BuildContext context) {
+  if (nameController.text == '' || hoursController.text == '' || globals.numDays == null) {
+    nameController.clear();
+    hoursController.clear();
+    globals.numDays = null;
+    final scaffoldErrorMessage = Scaffold.of(context);
+    scaffoldErrorMessage.showSnackBar(
+      SnackBar(
+        content: const Text('Please fill out all fields'),
+      ),
+    );
+    return;
+  }
   var now = new DateTime.now();
   int timeEachDay = ((60*double.tryParse(hoursController.text)) / globals.numDays).ceil();
   if (timeEachDay <= 0 || timeEachDay >= 1439) {
